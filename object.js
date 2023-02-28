d856e7ea40df0e4942f38c6b741e363a2ad0c138
github_username <you@domain.com>
2023-02-28 09:09:13 +0600

    method property used

--- object.js
+++ object.js
@@ -4,8 +4,22 @@ const student = {
     study :'math',
     subject: ['calculus', 'algebra', 'geometry'],
     exam: function(){
-       return this.money + 'This is value'
+       return this.money  + 'This is value'
+    },
+    improveExam : function(subject){
+        this.exam();
+        return `${this.name} us takeing improvement ${subject}`
+    },
+    treatDey: function(amount){
+        this.money = this.money - amount;
+        return this.money;
     }
 }
 const output = student.exam();
 console.log(output)
+
+const subjectAll  =student.improveExam('math')
+console.log(subjectAll);
+
+const treaM = student.treatDey(10);
+console.log(treaM);
