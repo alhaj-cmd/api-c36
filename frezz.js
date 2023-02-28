f9a5c49ef902b12e8045fa7786b414d7e4c5b89a
github_username <you@domain.com>
2023-02-28 09:24:10 +0600

    prometty and methods used

--- frezz.js
+++ frezz.js
@@ -1,3 +1,7 @@
 const bottle = { color: 'yello', price: 50, }
-const keys = object.keys(bottle);
+const keys = Object.keys(bottle);
 console.log(keys);
+const values = Object.values(bottle);
+console.log(values);
+const pair = Object.entries(bottle)
+console.log(pair);
