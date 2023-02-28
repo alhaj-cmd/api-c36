943f7f1822614e192bef9e49818d719d9613a4ca
github_username <you@domain.com>
2023-02-28 09:37:14 +0600

    frezze seal keys others used

--- frezz.js
+++ frezz.js
@@ -1,7 +1,11 @@
 const bottle = { color: 'yello', price: 50, }
-const keys = Object.keys(bottle);
-console.log(keys);
-const values = Object.values(bottle);
-console.log(values);
-const pair = Object.entries(bottle)
-console.log(pair);
+// const keys = Object.keys(bottle);
+// console.log(keys);
+// const values = Object.values(bottle);
+// console.log(values);
+// const pair = Object.entries(bottle)
+console.log(bottle);
+Object.seal(bottle)
+delete bottle.color;
+bottle.price = 1000;
+console.log(bottle)
